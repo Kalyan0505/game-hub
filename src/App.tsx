c13547e010ffd2ff789fa58b4ff042e09fd253cf
Kalyan0505 <kalyankumar1022@gmail.com>
2023-12-25 14:52:53 +0530

    Filter Games by genre

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,11 @@ import { Grid, GridItem, Show } from "@chakra-ui/react";
 import Navbar from "./components/Navbar";
 import GameGrid from "./components/GameGrid";
 import GenreList from "./components/GenreList";
+import { useState } from "react";
+import { Genre } from "./hooks/useGenres";
 
 function App() {
+  const [selectedGenre, setSelectedGenre] = useState<Genre | null>(null);
   return (
     <>
       <Grid
@@ -13,8 +16,8 @@ function App() {
           lg: `"nav nav" "aside main"`, //wider than 1024 px
         }}
         templateColumns={{
-          base: '1fr',
-          lg: '200px 1fr'
+          base: "1fr",
+          lg: "200px 1fr",
         }}
       >
         <GridItem area="nav">
@@ -22,12 +25,12 @@ function App() {
         </GridItem>
         <Show above="lg">
           <GridItem area="aside" paddingX={5}>
-            <GenreList />
+            <GenreList onSelectGenre={(genre) => setSelectedGenre(genre)} />
           </GridItem>
         </Show>
 
-        <GridItem area="main" >
-          <GameGrid />
+        <GridItem area="main">
+          <GameGrid selectedGenre={selectedGenre} />
         </GridItem>
       </Grid>
     </>
